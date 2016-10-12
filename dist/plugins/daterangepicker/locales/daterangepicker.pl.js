a0bd3a24a1cb2ebd1502a6ae91b175502f1b8ee4
Ionut Vornicescu <ionut.vornicescu@emag.ro>
2016-10-12 12:24:53 +0300

    Feature(DateRangePicker): Add locales

---
+++ dist/plugins/daterangepicker/locales/daterangepicker.pl.js
@@ -0,0 +1,16 @@
+// Start of local namespace
+;(function(window, undefined) {
+    'use strict';
+
+    window.daterangepicker_locale = {
+        applyLabel: 'Zastosuj',
+        cancelLabel: 'Anuluj',
+        fromLabel: 'Od',
+        toLabel: 'Do',
+        customRangeLabel: 'Zwyczaj',
+        weekLabel: 'T',
+        daysOfWeek: 'N_Pn_Wt_?r_Cz_Pt_So'.split('_'),
+        monthNames: 'stycze?_luty_marzec_kwiecie?_maj_czerwiec_lipiec_sierpie?_wrzesie?_pa?dziernik_listopad_grudzie?'.split('_'),
+        firstDay: 1
+    }
+}(window));
