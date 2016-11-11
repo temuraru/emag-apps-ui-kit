fd8ff24c3a08291c2eaffeccc102080fb1f74e1f
Ionut Vornicescu <ionut.vornicescu@emag.ro>
2016-11-11 12:58:29 +0200

    Improvement(Layout): Change texts

--- examples/_plu_ajax_modals.php
+++ examples/_plu_ajax_modals.php
@@ -24,7 +24,7 @@
                             <div class="row">
                                 <div class="col-md-8 col-md-offset-2">
                                     <div class="container-fluid">
-                                        <p>eMAG Apps Ui KIT</p>
+                                        <p>eMAG Apps UI KIT</p>
                                         <p><b>AJAX Modals</b></p>
                                     </div>
                                  </div>
