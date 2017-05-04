8392c11fdf626fa7b3cdeb9fc48b658be5314141
Eduard Stransky <stransky.eduard@gmail.com>
2017-05-04 16:05:20 +0300

    Feature(Layout): Added plugins versions page

---
+++ demo/plugins_versions.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en" class="js">
+<head>
+
+    <title>Layout - eMAG Apps UI KIT</title>
+    <?php include_once "modules/_mod_meta.php"?>
+    <?php include_once "modules/_mod_top_include.php"?>
+    <!-- PLUGIN: PRISM: This plugin helps display demo code. Don't add it everywhere -->
+    <link rel="stylesheet" href="../dist/plugins/prism/prism.min.css">
+
+</head>
+<?php include_once "modules/_mod_browser_upgrade.php"?>
+
+<!-- HEADER:Start -->
+<?php include_once "modules/_mod_header.php"?>
+<!-- HEADER:End -->
+
+<!-- PAGE:Start -->
+<div class="main-container" id="main-container">
+    <div class="main-container-inner">
+        <!-- SIDEBAR:Start -->
+        <?php include_once "modules/_mod_sidebar.php"?>
+        <!-- SIDEBAR:End -->
+        <!-- CONTENT:Start -->
+        <div class="main-content">
+            <div class="page-content">
+                <div class="jumbotron content-header">
+                    <div class="container-fluid">
+                        <p>eMAG Apps UI KIT</p>
+                        <p><b>Technical details</b></p>
+                    </div>
+                </div>
+                <section id="boilerplate" class="pad-top-40">
+                    <div class="panel">
+                        <div class="panel-body">
+
+                            <h2><strong>Plugins versions</strong></h2>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-6">
+
+
+                                    <table class="table table-bordered mg-top-30">
+                                        <thead>
+                                        <tr>
+
+                                            <th>Name</th>
+                                            <th>Version</th>
+                                            <th>Observations</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr>
+                                            <td><a href="js_components.php#js-block-ui">BlockUI</a></td>
+                                            <td><span class="label label-info">2.70.0</span></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Bootstrap multiselect</td>
+                                            <td><span class="label label-info">2.0</span></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td><a href="js_components.php#js-custom-notifications">Bootstrap notify</a></td>
+                                            <td><span class="label label-info">3.1.5</span></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td><a href="plugin_stats.php#charts">Chart</a></td>
+                                            <td><span class="label label-info">2.5.0</span></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td><a href="js_complex_form_elements.php#jsChosenJsAutocomplete">Chosen</a></td>
+                                            <td><span class="label label-info">1.4.2</span></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Countdown<br>Using: Moment-timezone</td>
+                                            <td><span class="label label-info">2.2.0</span><br><span class="label label-info">0.5.5</span></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td><a href="plugin_datetime.php#dateRangePicker">Daterangepicker</a></td>
+                                            <td><span class="label label-info">2.1.24</span></td>
+                                            <td><span class="label label-warning">modified</span></td>
+                                        </tr>
+                                        <tr>
+                                            <td><a href="plugin_datetime.php">Datetimepicker</a></td>
+                                            <td><span class="label label-info">4.17.37</span></td>
+                                            <td><span class="label label-warning">modified</span></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Drop</td>
+                                            <td><span class="label label-info">1.4.1</span></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Dropzone</td>
+                                            <td><span class="label label-info">4.2.0</span></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td><a href="plugin_fancytree.php">Fancytree</a><br>Using: jquery.fancytree.filter.js</td>
+                                            <td><span class="label label-info">2.15</span><br><span class="label label-info">2.22.0</span></td>
+                                            <td><span class="label label-warning">modified</span></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Hierarchicalselect</td>
+                                            <td><span class="label label-info">1.0.0</span></td>
+                                            <td><span class="label label-default">internal development</span></td>
+                                        </tr>
+                                        <tr>
+                                            <td><a href="js_complex_form_elements.php#jsTextareaWithInputLimiter">Input Limiter</a></td>
+                                            <td><span class="label label-info">1.3.1</span></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td><a href="plugin_introjs.php">Intro</a></td>
+                                            <td><span class="label label-info">2.3.0</span></td>
+                                            <td><span class="label label-warning">modified</span></td>
+                                        </tr>
+                                        <tr>
+                                            <td><a href="plugin_jqgrid.php">jqGrid</a></td>
+                                            <td><span class="label label-info">5.0.1</span></td>
+                                            <td><span class="label label-warning">modified</span> <span class="label label-warning">wrapper</span></td>
+                                        </tr>
+                                        <tr>
+                                            <td>jQuery Cookie</td>
+                                            <td><span class="label label-info">1.4.1</span></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td><a href="image_gallery.php">Magnific</a></td>
+                                            <td><span class="label label-info">1.0.0</span></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Nprogress</td>
+                                            <td><span class="label label-info">0.2.0</span></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Prism</td>
+                                            <td><span class="label label-info">1.6.0</span></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td><a href="js_complex_form_elements.php#jsSelectizeTagInput">Selectize</a></td>
+                                            <td><span class="label label-info">0.12.1</span></td>
+                                            <td><span class="label label-warning">modified</span></td>
+                                        </tr>
+
+                                        <tr>
+                                            <td><a href="plugin_jqgrid.php#jqGrid-with-sticky-panels">Sticky-kit</a></td>
+                                            <td><span class="label label-info">1.1.2</span></td>
+                                            <td><span class="label label-warning">modified</span></td>
+                                        </tr>
+                                        <tr>
+                                            <td><a href="plugin_wysiwyg.php">Summernote</a></td>
+                                            <td><span class="label label-info">0.7.3</span></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Tether</td>
+                                            <td><span class="label label-info">1.1.0</span></td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td><a href="plugin_wizard.php">Wizard</a></td>
+                                            <td><span class="label label-info">3.16.0</span></td>
+                                            <td><span class="label label-warning">modified</span></td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+        <!-- CONTENT:End -->
+        <!-- FOOTER:Start -->
+        <?php include_once "modules/_mod_footer.php"?>
+        <!-- FOOTER:End -->
+    </div>
+</div>
+<!-- PAGE:End -->
+
+<!-- SCRIPTS:Start -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" data-dependency-name="jquery"></script>
+<script>window.jQuery || document.write("<script src=\"../dist/js/lib/jquery-1.11.3.min.js\">"+"<"+"/script>")</script>
+<script src="../dist/js/main_script.min.js" data-dependency-name="main_script"></script>
+<script src="../dist/js/demo_helpers.js"></script>
+
+<!-- PLUGIN: PRISM: This plugin helps display demo code. Don't add it everywhere -->
+<script src="../dist/plugins/prism/prism.min.js"></script>
+<!-- SCRIPTS:End -->
+
+<!-- DOCUMENT-READY:Start -->
+<script type="text/javascript">
+    $(document).ready(function () {
+        console.log('Ready, Captain!');
+
+        demoHelpers();
+    });
+</script>
+<!-- DOCUMENT-READY:End -->
+
+</body>
+</html>
